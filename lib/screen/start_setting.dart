9a003ee60e1bfbeea0162d64331f41d0edcfa5d4
Yoochanhong <chanhongy6@naver.com>
2022-11-19 00:01:57 +0900

    배터리 잔량 커스텀 라디오버튼 추가

--- lib/screen/start_setting.dart
+++ lib/screen/start_setting.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
 
-class StartSettingScreen extends StatelessWidget {
+enum Battery { low, middle, high, full }
+
+class StartSettingScreen extends StatefulWidget {
   const StartSettingScreen({Key? key}) : super(key: key);
 
   @override
+  State<StartSettingScreen> createState() => _StartSettingScreenState();
+}
+
+class _StartSettingScreenState extends State<StartSettingScreen> {
+  Battery? battery = Battery.low;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  const Icon(Icons.battery_2_bar),
+                  Radio<Battery>(
+                      value: Battery.low,
+                      groupValue: battery,
+                      onChanged: (Battery? value) {
+                        setState(() {
+                          battery = value;
+                        });
+                      }),
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.battery_4_bar),
+                  Radio<Battery>(
+                      value: Battery.middle,
+                      groupValue: battery,
+                      onChanged: (Battery? value) {
+                        setState(() {
+                          battery = value;
+                        });
+                      }),
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.battery_5_bar),
+                  Radio<Battery>(
+                      value: Battery.high,
+                      groupValue: battery,
+                      onChanged: (Battery? value) {
+                        setState(() {
+                          battery = value;
+                        });
+                      }),
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(Icons.battery_std),
+                  Radio<Battery>(
+                      value: Battery.full,
+                      groupValue: battery,
+                      onChanged: (Battery? value) {
+                        setState(() {
+                          battery = value;
+                        });
+                      }),
+                ],
+              ),
+            ],
+          ),
+          Row(),
+        ],
+      ),
+    );
   }
 }
