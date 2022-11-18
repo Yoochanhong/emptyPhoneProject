0cfdc54c4da90cde582f8f162959053ab6dc5bf0
Yoochanhong <chanhongy6@naver.com>
2022-11-18 15:05:55 +0900

    시간, 요일 추가

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
+  String getSystemTime() {
+    DateFormat dateFormat = DateFormat("h:mm");
+    String _dateTime = dateFormat.format(DateTime.now());
+    return _dateTime;
+  }
 
-class _MainScreenState extends State<MainScreen> {
+  String getSystemDate() {
+    DateFormat dateFormat = DateFormat("MM월 dd일 E요일");
+    String _dateTime = dateFormat.format(DateTime.now());
+    return _dateTime;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,11 +29,56 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/wallpaper.jpeg'),
-            fit: BoxFit.fill,
-          ),
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/wallpaper.jpeg'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 30,
+              left: 120,
+              child: Container(
+                color: Colors.transparent,
+                child: TimerBuilder.periodic(
+                  Duration(seconds: 1),
+                  builder: (context) {
+                    return Text(
+                      getSystemTime(),
+                      style: TextStyle(
+                        fontSize: 65,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              top: 105,
+              left: 133,
+              child: Container(
+                color: Colors.transparent,
+                child: TimerBuilder.periodic(
+                  Duration(seconds: 1),
+                  builder: (context) {
+                    return Text(
+                      getSystemDate(),
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
