8b4e3a8fcab6903f7ab44096bf3f0061d528c282
Yoochanhong <chanhongy6@naver.com>
2022-11-18 20:35:11 +0900

    시작 세팅페이지 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:empty_phone_project/screen/main_screen.dart';
+import 'package:empty_phone_project/screen/start_setting.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+      home: StartSettingScreen(),
     );
   }
 }
---
+++ lib/screen/start_setting.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartSettingScreen extends StatelessWidget {
+  const StartSettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
