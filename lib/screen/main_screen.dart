e7c66005a5cb25ff6e2cc720dbd0eb8be4113199
Yoochanhong <chanhongy6@naver.com>
2022-11-18 17:11:23 +0900

    상단 바 없애기

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:timer_builder/timer_builder.dart';
 
@@ -36,142 +37,144 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: Stack(
-            children: [
-              Expanded(
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/wallpaper.jpeg'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 10,
+              left: 10,
+              child: Text(
+                'KT',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            Positioned(
+              right: 10,
+              top: 5,
+              child: Row(
+                children: [
+                  Icon(Icons.bluetooth, size: 15),
+                  SizedBox(width: 3),
+                  Icon(Icons.wifi_tethering, size: 15),
+                  SizedBox(width: 3),
+                  Text('LTE',
+                      style:
+                          TextStyle(fontSize: 11, fontWeight: FontWeight.bold)),
+                  SizedBox(width: 5),
+                  Text('56%', style: TextStyle(fontSize: 12)),
+                  Icon(Icons.battery_4_bar, size: 15),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Container(
+                    color: Colors.transparent,
+                    child: Icon(Icons.lock),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Center(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: TimerBuilder.periodic(
+                        Duration(seconds: 1),
+                        builder: (context) {
+                          return Text(
+                            getSystemTime(),
+                            style: TextStyle(
+                              fontSize: 60,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    color: Colors.transparent,
+                    child: TimerBuilder.periodic(
+                      Duration(seconds: 1),
+                      builder: (context) {
+                        return Text(
+                          getSystemDate() + ' ' + getWeek(),
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 15,
+              left: 20,
+              child: Opacity(
+                opacity: 0.4,
                 child: Container(
+                  width: 40,
+                  height: 40,
                   decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/wallpaper.jpeg'),
-                      fit: BoxFit.fill,
-                    ),
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
                   ),
                 ),
               ),
-              Positioned(
-                top: 6,
-                left: 6,
-                child: Text(
-                  'KT',
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
+            ),
+            Positioned(
+              bottom: 22,
+              left: 27,
+              child: Icon(
+                Icons.call,
+                color: Colors.white,
               ),
-              Positioned(
-                right: 10,
-                top: 5,
-                child: Row(
-                  children: [
-                    Icon(Icons.bluetooth, size: 15),
-                    SizedBox(width: 3),
-                    Icon(Icons.wifi_tethering, size: 15),
-                    SizedBox(width: 3),
-                    Text('LTE',
-                        style: TextStyle(
-                            fontSize: 11, fontWeight: FontWeight.bold)),
-                    SizedBox(width: 5),
-                    Text('56%', style: TextStyle(fontSize: 12)),
-                    Icon(Icons.battery_4_bar, size: 15),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 30,
-                left: 175,
+            ),
+            Positioned(
+              bottom: 15,
+              right: 20,
+              child: Opacity(
+                opacity: 0.4,
                 child: Container(
-                  color: Colors.transparent,
-                  child: Icon(Icons.lock),
-                ),
-              ),
-              Positioned(
-                top: 110,
-                left: 125,
-                child: Container(
-                  color: Colors.transparent,
-                  child: TimerBuilder.periodic(
-                    Duration(seconds: 1),
-                    builder: (context) {
-                      return Text(
-                        getSystemTime(),
-                        style: TextStyle(
-                          fontSize: 60,
-                        ),
-                      );
-                    },
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
                   ),
                 ),
               ),
-              Positioned(
-                top: 175,
-                left: 135,
-                child: Container(
-                  color: Colors.transparent,
-                  child: TimerBuilder.periodic(
-                    Duration(seconds: 1),
-                    builder: (context) {
-                      return Text(
-                        getSystemDate() + ' ' + getWeek(),
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      );
-                    },
-                  ),
-                ),
+            ),
+            Positioned(
+              bottom: 22,
+              right: 27,
+              child: Icon(
+                Icons.camera_alt_sharp,
+                color: Colors.white,
               ),
-              Positioned(
-                bottom: 15,
-                left: 20,
-                child: Opacity(
-                  opacity: 0.4,
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 22,
-                left: 27,
-                child: Icon(
-                  Icons.call,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                bottom: 15,
-                right: 20,
-                child: Opacity(
-                  opacity: 0.4,
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 22,
-                right: 27,
-                child: Icon(
-                  Icons.camera_alt_sharp,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
