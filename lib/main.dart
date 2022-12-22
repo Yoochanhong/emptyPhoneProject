138343a8300fd7fd72e4ddf1a24c3944fd2f6f9a
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-22 18:54:38 +0900

    const 최적화

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:empty_phone_project/screen/main_screen.dart';
 import 'package:empty_phone_project/screen/start_setting.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: StartSettingScreen(),
     );
