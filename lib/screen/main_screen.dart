94e5cd9a3a5770875cdd47aa95f5c531adad771c
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-22 19:06:51 +0900

    코드 정리

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -14,20 +14,20 @@ class MainScreen extends StatelessWidget {
 
   String getSystemTime() {
     DateFormat dateFormat = DateFormat("h:mm");
-    String _dateTime = dateFormat.format(DateTime.now());
-    return _dateTime;
+    String dateTime = dateFormat.format(DateTime.now());
+    return dateTime;
   }
 
   String getSystemDate() {
     DateFormat dateFormat = DateFormat("MM월 dd일");
-    String _dateTime = dateFormat.format(DateTime.now());
-    return _dateTime;
+    String dateTime = dateFormat.format(DateTime.now());
+    return dateTime;
   }
 
   String getWeek() {
     DateTime now = DateTime.now();
     String? getDay = week[DateFormat('E').format(now)];
-    String dateStr = DateFormat(getDay! + '요일').format(now);
+    String dateStr = DateFormat('${getDay!}요일').format(now);
     return dateStr;
   }
 
@@ -67,153 +67,151 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                if (imageFile == null)
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Image.asset(
-                      'assets/wallpaper.jpeg',
-                      fit: BoxFit.fill,
-                    ),
-                  )
-                else
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Image.file(File(imageFile!.path)),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              if (imageFile == null)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: Image.asset(
+                    'assets/wallpaper.jpeg',
+                    fit: BoxFit.fill,
                   ),
-                Positioned(
-                  top: 10,
-                  left: 10,
-                  child: Text(
-                    radioStateInfo.company,
-                    style: const TextStyle(
-                      fontSize: 12,
-                    ),
+                )
+              else
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: Image.file(File(imageFile!.path)),
+                ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Text(
+                  radioStateInfo.company,
+                  style: const TextStyle(
+                    fontSize: 12,
                   ),
                 ),
-                Positioned(
-                  right: 10,
-                  top: 10,
-                  child: Row(
-                    children: [
-                      const Icon(Icons.bluetooth, size: 15),
-                      const SizedBox(width: 2),
-                      const Icon(Icons.place, size: 15),
-                      const SizedBox(width: 2),
-                      const Text('LTE',
-                          style: TextStyle(
-                              fontSize: 11, fontWeight: FontWeight.bold)),
-                      const SizedBox(width: 3),
-                      const Icon(Icons.signal_cellular_alt, size: 15),
-                      const SizedBox(width: 3),
-                      Text(batteryPercentInfo(),
-                          style: const TextStyle(fontSize: 12)),
-                      Icon(batteryStateInfo(), size: 15),
-                    ],
-                  ),
-                ),
-                Column(
+              ),
+              Positioned(
+                right: 10,
+                top: 10,
+                child: Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30),
-                      child: Container(
-                        color: Colors.transparent,
-                        child: const Icon(Icons.lock),
-                      ),
-                    ),
-                    Center(
-                      child: Container(
-                        padding: const EdgeInsets.only(top: 100),
-                        color: Colors.transparent,
-                        child: TimerBuilder.periodic(
-                          const Duration(seconds: 1),
-                          builder: (context) {
-                            return Text(
-                              getSystemTime(),
-                              style: const TextStyle(
-                                fontSize: 60,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: Container(
-                        color: Colors.transparent,
-                        child: TimerBuilder.periodic(
-                          const Duration(seconds: 1),
-                          builder: (context) {
-                            return Text(
-                              getSystemDate() + ' ' + getWeek(),
-                              style: const TextStyle(
-                                fontSize: 16,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
+                    const Icon(Icons.bluetooth, size: 15),
+                    const SizedBox(width: 2),
+                    const Icon(Icons.place, size: 15),
+                    const SizedBox(width: 2),
+                    const Text('LTE',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.bold)),
+                    const SizedBox(width: 3),
+                    const Icon(Icons.signal_cellular_alt, size: 15),
+                    const SizedBox(width: 3),
+                    Text(batteryPercentInfo(),
+                        style: const TextStyle(fontSize: 12)),
+                    Icon(batteryStateInfo(), size: 15),
                   ],
                 ),
-                Positioned(
-                  bottom: 15,
-                  left: 20,
-                  child: Opacity(
-                    opacity: 0.4,
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
                     child: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white,
+                      color: Colors.transparent,
+                      child: const Icon(Icons.lock),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 100),
+                      color: Colors.transparent,
+                      child: TimerBuilder.periodic(
+                        const Duration(seconds: 1),
+                        builder: (context) {
+                          return Text(
+                            getSystemTime(),
+                            style: const TextStyle(
+                              fontSize: 60,
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
-                ),
-                const Positioned(
-                  bottom: 22,
-                  left: 27,
-                  child: Icon(
-                    Icons.call,
-                    color: Colors.white,
-                  ),
-                ),
-                Positioned(
-                  bottom: 15,
-                  right: 20,
-                  child: Opacity(
-                    opacity: 0.4,
+                  Center(
                     child: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white,
+                      color: Colors.transparent,
+                      child: TimerBuilder.periodic(
+                        const Duration(seconds: 1),
+                        builder: (context) {
+                          return Text(
+                            '${getSystemDate()} ${getWeek()}',
+                            style: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
-                ),
-                const Positioned(
-                  bottom: 22,
-                  right: 27,
-                  child: Icon(
-                    Icons.camera_alt_sharp,
-                    color: Colors.white,
+                ],
+              ),
+              Positioned(
+                bottom: 15,
+                left: 20,
+                child: Opacity(
+                  opacity: 0.4,
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+              const Positioned(
+                bottom: 22,
+                left: 27,
+                child: Icon(
+                  Icons.call,
+                  color: Colors.white,
+                ),
+              ),
+              Positioned(
+                bottom: 15,
+                right: 20,
+                child: Opacity(
+                  opacity: 0.4,
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const Positioned(
+                bottom: 22,
+                right: 27,
+                child: Icon(
+                  Icons.camera_alt_sharp,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
